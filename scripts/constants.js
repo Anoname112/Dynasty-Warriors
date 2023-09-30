b15be7ba3ebfa69b7eeeea53a5fdbbb623b057c5
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2023-09-30 08:49:45 +0700

    Update constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -8,21 +8,22 @@ const guideSvg = '<svg xmlns="http://www.w3.org/2000/svg" fill="#FFF" viewBox="0
 const checkboxSvg = '<svg xmlns="http://www.w3.org/2000/svg" fill="#FFF" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg>';
 
 const warriors = {
-	'Yukimura': [
+	'Zhao Yun': [
 		{
-			'Game': "SW",
-			'Name': "Susano",
-			'Rank': 5,
-			'Base': 47,
-			'Element': "Fire",
+			'Game': "DW5",
+			'Name': "Fierce Dragon",
+			'Rank': 4,
+			'Base': 36,
+			'Element': "Medium",
 			'Stats': [
-				"Life: 50",
-				"Musou: 27",
-				"Attack: 40",
-				"Defense: 38"
+				"Charge: 16",
+				"Life: 17",
+				"Musou: 17",
+				"Attack: 17",
+				"Defense: 17"
 			],
-			'Stage': "Siege of Osaka",
-			'Requirement': "Complete all the main missions and defeat 1000 enemies. The supply team will appear in the southwest and head towards the middle eastern gate."
+			'Stage': "Battle of Cheng Du",
+			'Requirement': "Defeat Liu Xun's three sub-generals then defeat Liu Xun himself, and capture Luo Castle."
 		}, {
 			'Game': "SW",
 			'Name': "Rakan",
