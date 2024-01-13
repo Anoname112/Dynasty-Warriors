6c62c8b5f6170beb2f7b84d4dbf2cef6c8ac2333
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2024-01-13 23:44:15 +0700

    Update constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -2687,7 +2687,7 @@ const warriors = {
 				"Mounted Attack: 46"
 			],
 			'Stage': "Guan Du (Yuan Shao’s Forces)",
-			'Requirement': "Keep Wen Chou alive for 10 minutes. Go to his aid at the beginning of the battle, he will be in trouble quickly. As soon as he is safe, find and defeat Guan Yu (if Guan Yu gets near Wen Chou, Guan Yu will automatically defeat Wen Chou). After defeating Guan Yu return to Wen Chou and start killing the closest officers."
+			'Requirement': "Keep Wen Chou alive for 10 minutes. Go to his aid at the beginning of the battle, he will be in trouble quickly. As soon as he is safe, find and defeat Guan Yu (if Guan Yu gets near Wen Chou, Guan Yu will automatically kill Wen Chou). After defeating Guan Yu, return to Wen Chou and continue to protect him."
 		}, {
 			'Game': "DW3",
 			'Name': "Imperial Saber",
