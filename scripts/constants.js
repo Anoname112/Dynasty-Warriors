79ffc41a7b11e975f8ed7f4adb1b0fb6b46fc66c
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2024-01-21 06:48:24 +0700

    Update constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -1996,7 +1996,7 @@ const warriors = {
 				"Luck: 26",
 				"Musou Charge: 28"
 			],
-			'Stage': "The Battle at He Fei (Sun Quan’s Forces)",
+			'Stage': "The Battle at He Fei (Sun Quan's Forces)",
 			'Requirement': "Wait for Taishi Ci’s death event. This event occurs when he meets Zhang Liao, which will be early in the battle. Do not defeat Zhang Liao before this event."
 		}, {
 			'Game': "DW3",
